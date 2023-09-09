7f1a2076abcb7fec74405a924729a816787ead8d
ais-one <aaronjxz@gmail.com>
2023-09-09 10:07:15 +0800

    wip

--- router/index.js
+++ router/index.js
@@ -5,10 +5,6 @@ const router = require('express').Router()
 module.exports = function (app) {
   app.use('/api',
     router.use('/', require('./base')),
-    // router.use('/auth', require('./auth').myauthRoute),
-    // router.use('/oidc', require('./auth').oidcRoute),
-    // router.use('/oauth', require('./auth').oauthRoute),
-    // router.use('/saml', require('./auth').samlRoute),
     router.use('/t4t', require('./t4t')),
   )
 }
