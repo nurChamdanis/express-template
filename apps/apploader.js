7bf69e665ba776defb3403584c2c1ca36bf8dc9d
ais-one <aaronjxz@gmail.com>
2023-09-09 10:14:55 +0800

    wip

--- apps/apploader.js
+++ apps/apploader.js
@@ -6,12 +6,13 @@
 
 // routes to your custom application here
 module.exports = (app) => {
-  require('./app-sample/routes')({ app, routePrefix: '/api/app-sample'})
-
-  // authentication stuff Below
-  // routes used are: /api/auth (own auth rollout), /api/oauth, /api/oidc, /api/saml
-  require('./app-auth')({ app, routePrefix: '/api'})
-
   // your can add more routes here ensure no clash in urlPrefix
   // require('./app-2nd/routes')({ app, urlPrefix: '/api/app-second'})
+
+  // some sample/demo routes - you can remove if not needed
+  require('./app-sample/routes')({ app, routePrefix: '/api/app-sample'})
+
+  // authentication stuff Below - you can remove if not needed (be aware of routing if you are customizing your auth)
+  // routes used are: /api/auth (own auth rollout), /api/oauth, /api/oidc, /api/saml
+  require('./app-auth')({ app, routePrefix: '/api'})
 }
