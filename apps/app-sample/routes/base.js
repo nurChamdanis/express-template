1b7dd5a2162a26f1023994d5d22df6213052720f
ais-one <aaronjxz@gmail.com>
2023-09-13 14:20:30 +0800

    db test route

--- apps/app-sample/routes/base.js
+++ apps/app-sample/routes/base.js
@@ -1,7 +1,25 @@
 'use strict'
 
 const express = require('express')
+const s = require('@es-labs/node/services')
 
 module.exports = express.Router()
-  .get('/', (req, res) => res.send('app-sample OK'))
-  .get('/healthcheck', (req, res) => res.send('app-sample/healthcheck OK'))
+  .get('/', (req, res) => res.send({ status: 'app-sample OK' }))
+  .get('/healthcheck', (req, res) => res.send({ status: 'app-sample/healthcheck OK' }))
+  .get('/check-db', async (req, res) => {
+    const connectionName = req.query.conn || 'knex1'
+    const connQuery = req.query.sql || 'SELECT 1'
+    try {
+      const clientName = s.get(connectionName).context.client.config.client
+      // const a = s.get('knex1').context.client.config.client
+      // console.log(a, a.config, a.database, a.context.client.config.client)
+      const rv = await s.get(connectionName).raw(connQuery)
+      return res.status(200).json({
+        connectionName,
+        db: clientName
+      })
+    } catch (e) {
+      console.log(e)
+      return res.status(500).json(e)
+    }
+  })
